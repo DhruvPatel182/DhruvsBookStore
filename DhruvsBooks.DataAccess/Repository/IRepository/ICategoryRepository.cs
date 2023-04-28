10fa9c8349af50faa09391e5dd2859ff0e95ab6d
DhruvPatel182 <dp32517263@gmail.com>
2023-04-28 14:22:48 -0400

    Second Push Part-2

---
+++ DhruvsBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using DhruvsBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DhruvsBooks.DataAccess.Repository.IRepository
+{
+   public  interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
